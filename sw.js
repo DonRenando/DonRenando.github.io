2afb8dff8a86b1e1b9b54b4fd6dc295cf5ffacfd
DonRenando <renan.husson@hotmail.fr>
2026-06-15 15:40:09 +0200

    Add manifest and icon files to service worker cache

--- sw.js
+++ sw.js
@@ -4,7 +4,10 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll([
       './',
-      './index.html'
+      './index.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png'
     ]))
   );
 });
